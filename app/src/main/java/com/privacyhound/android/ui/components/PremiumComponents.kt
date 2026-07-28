52384beeb70a4c9aed941910c2e9a5496f43c450
root <root@localhost.localdomain>
2026-07-29 04:34:26 +0600

    Fix missing overlay colors and height import

--- app/src/main/java/com/privacyhound/android/ui/components/PremiumComponents.kt
+++ app/src/main/java/com/privacyhound/android/ui/components/PremiumComponents.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
